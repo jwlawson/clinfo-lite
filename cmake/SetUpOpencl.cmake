952d8f48d575c50b01083b23e29bfac1254b330b
John Lawson <john@jwlawson.co.uk>
2020-02-23 13:57:44 +0000

    Add GitHub Action workflow (#1)
    * Add GitHub Action workflow
    
    * Actually set cache vars rather than declare them
    
    * Handle case where no platforms are found

--- cmake/SetUpOpencl.cmake
+++ cmake/SetUpOpencl.cmake
@@ -1,7 +1,8 @@
 # Copyright 2019-2020 John Lawson
 # Available for use under the BSD 3-Clause Clear License.
 
-if(NOT FORCE_DOWNLOAD_OPENCL)
+if(OpenCL_LIBRARY OR NOT FORCE_DOWNLOAD_OPENCL)
+  # Find system OpenCL, or previously set up external OpenCL project
   find_package(OpenCL QUIET)
 endif()
 
@@ -16,17 +17,24 @@ if(NOT OpenCL_FOUND AND DOWNLOAD_MISSING_DEPS)
   )
   ExternalProject_Get_Property(opencl_headers SOURCE_DIR)
   file(MAKE_DIRECTORY ${SOURCE_DIR})
-  set(OpenCL_INCLUDE_DIR ${SOURCE_DIR} CACHE PATH "OpenCL header directory")
+  set(OpenCL_INCLUDE_DIR ${SOURCE_DIR}
+    CACHE PATH "OpenCL header directory" FORCE
+  )
+  message(STATUS "inlcude_dir: ${OpenCL_INCLUDE_DIR}")
 
   ExternalProject_Add(opencl_icd_loader
     GIT_REPOSITORY  https://github.com/KhronosGroup/OpenCL-ICD-Loader
     GIT_TAG         master
     DEPENDS         opencl_headers
     CMAKE_ARGS      -DOPENCL_ICD_LOADER_HEADERS_DIR=${OpenCL_INCLUDE_DIR}
+                    -DBUILD_TESTING=OFF
     INSTALL_COMMAND ""
   )
   ExternalProject_Get_Property(opencl_icd_loader BINARY_DIR)
-  set(OpenCL_LIBRARY ${BINARY_DIR}/libOpenCL.so CACHE PATH "OpenCL library location")
+  set(OpenCL_LIBRARY ${BINARY_DIR}/libOpenCL.so
+    CACHE PATH "OpenCL library location" FORCE
+  )
+  message(STATUS "library: ${OpenCL_LIBRARY}")
 
   find_package(OpenCL REQUIRED)
   add_dependencies(OpenCL::OpenCL opencl_headers opencl_icd_loader)
